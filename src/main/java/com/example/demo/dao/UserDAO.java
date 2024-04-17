58457fb9ba05d49facfe0d0897a283a5ed9c6339
Gokulprasath-N <gokulkumargn@gmail.com>
2024-04-17 14:21:47 +0530

    third commit

--- src/main/java/com/example/demo/dao/UserDAO.java
+++ src/main/java/com/example/demo/dao/UserDAO.java
@@ -1,13 +1,12 @@
 package com.example.demo.dao;
 
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.model.User;
-//import com.example.demo.model.UserDynamo;
 
 
-@EnableScan
+
 public interface UserDAO extends CrudRepository <User, Integer>{
 
 }
